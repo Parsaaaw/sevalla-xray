25f54653a3e44f6702a8fb36a42554bd4470050c
Parsaw <115087768+Parsaaaw@users.noreply.github.com>
2026-08-19 10:11:46 -0700

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+PORT="${PORT:-8080}"
+sed -e "s/__PORT__/${PORT}/g" \
+    -e "s/__UUID__/${XRAY_UUID}/g" \
+    -e "s/__WSPATH__/${WS_PATH:-ray}/g" \
+    /etc/xray/config.json.template > /etc/xray/config.json
+exec xray run -config /etc/xray/config.json
